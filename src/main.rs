58c53e742167aac48c07bd7931618e4e9f51d324
Sean Hellum <seanhellum45@gmail.com>
2020-02-09 20:10:49 +0000

    add project files

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
-fn index() {
-    //
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use]
+extern crate rocket;
+use rocket::response::NamedFile;
+use std::io::Result;
+use std::path::{Path, PathBuf};
+
+#[get("/")]
+fn index() -> Result<NamedFile> {
+    NamedFile::open("site/index.html")
 }
+
+#[get("/www/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("www/").join(file)).ok()
+}
+
 fn main() {
 
     rocket::ignite()
-      .mount("/hello", routes![hello])
+      .mount("/hello", routes![index, files(file: PathBuf)])
       .launch();
 }
